3967e5a8cd37a6ea0e0df51a6d0d5071f374fce0
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-07-02 22:51:21 +0200

    Add initial sources

---
+++ test/Atata.Reqnroll.NUnit.IntegrationTests/GlobalFixture.cs
@@ -0,0 +1,13 @@
+﻿[assembly: SetCulture("en-US")]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+namespace Atata_Reqnroll_NUnit_IntegrationTests;
+
+public sealed class GlobalFixture : AtataGlobalFixture
+{
+    protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
+        globalProperties.UseRootNamespaceOf<GlobalFixture>();
+
+    protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder) =>
+        builder.LogConsumers.AddNLogFile();
+}
