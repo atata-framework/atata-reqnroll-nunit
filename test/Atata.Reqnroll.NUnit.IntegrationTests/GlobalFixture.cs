fe92b4e28b54a1153e2e46a4c12f37a594a0896a
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-10-16 21:59:54 +0200

    Refactor `GlobalFixture`

--- test/Atata.Reqnroll.NUnit.IntegrationTests/GlobalFixture.cs
+++ test/Atata.Reqnroll.NUnit.IntegrationTests/GlobalFixture.cs
@@ -1,10 +1,7 @@
-namespace Atata.Reqnroll.NUnit.IntegrationTests;
+﻿namespace Atata.Reqnroll.NUnit.IntegrationTests;
 
 public sealed class GlobalFixture : AtataGlobalFixture
 {
-    protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
-        globalProperties.UseRootNamespaceOf<GlobalFixture>();
-
     protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder) =>
         builder.LogConsumers.AddNLogFile();
 }
